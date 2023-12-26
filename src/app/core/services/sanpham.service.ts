11c15a120e8b5b94ba6d634530b62452af17ff23
DaoHien <hien@gmail.com>
2023-12-26 23:10:52 +0700

    update

--- src/app/core/services/sanpham.service.ts
+++ src/app/core/services/sanpham.service.ts
@@ -6,7 +6,8 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class SanphamService {
-  private base_url = environment.BASE_API;
+  // private base_url = environment.BASE_API;
+  public base_url = "https://localhost:44313";
   constructor(private httpClient: HttpClient) { }
   getSanPham(page: number){
     return this.httpClient.get(this.base_url + '/api/SanPham/get-all-sanpham');
