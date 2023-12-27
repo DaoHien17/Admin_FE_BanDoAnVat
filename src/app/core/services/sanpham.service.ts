42f7c665886d7d7dcce0c734d2a67a71187c5659
DaoHien <hien@gmail.com>
2023-12-27 23:18:58 +0700

    dev/loaisanpham

--- src/app/core/services/sanpham.service.ts
+++ src/app/core/services/sanpham.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
 })
 export class SanphamService {
   // private base_url = environment.BASE_API;
-  public base_url = "https://localhost:44313";
+  public base_url = "https://localhost:3000";
   constructor(private httpClient: HttpClient) { }
   getSanPham(page: number){
     return this.httpClient.get(this.base_url + '/api/SanPham/get-all-sanpham');
--- src/app/main/product/loaisp/loaisp.component.ts
+++ src/app/main/product/loaisp/loaisp.component.ts
@@ -13,57 +13,161 @@ declare var $: any;
   templateUrl: './loaisp.component.html',
   styleUrls: ['./loaisp.component.css']
 })
-export class LoaispComponent implements OnInit {
-  // public loaisp:any;
-  // public list_loaisanphams: any;
-  // p: number = 1;
-  // total: number = 0;
-  // public frmSanPham!: FormGroup;
-  // public frmSearch!: FormGroup;
-  // public loaiQuyen: string = 'Admin';
-  // public file: any;
-  // public showUpdateModal: any = false;
-  // public doneSetupForm: any;
+export class LoaispComponent extends BaseComponent implements OnInit, AfterViewInit {
+  public list_sanphams: any;
+  public list_nhacungcaps: any;
+  public list_loaisanphams: any;
+  p: number = 1;
+  total: number = 0;
 
-  // frmloaisp!: FormGroup
-  // public updateModal: any;
-  // public setupForm: any;
-  // public submitted = false;
+  public isCreate = false;
+  public sanpham: any;
 
-
-  // constructor(injector: Injector, private service: LoaispComponent) {
-  //   super(injector);
-  //   }
-  ngOnInit(): void {
-    // this.getLoaiSanPham();
-    // this.frmloaisp = new FormGroup({
-    //   'txt_maloaisp': new FormControl(''),
-    //   'txt_tenloaisp': new FormControl(''),
-
-    // });
+  public frmLoaiSanPham!: FormGroup;
+  public frmSearch: FormGroup;
+  public loaiQuyen: string = 'Admin';
+  public file: any;
+  public showUpdateModal: any = false;
+  public doneSetupForm: any;
+  constructor(injector: Injector, private service: SanphamService) {
+    super(injector);
+    this.frmSearch = new FormGroup({
+      'txt_tenlsp': new FormControl('', []),
+    });
   }
-  // get maloaisp() {
-  //   return this.frmloaisp.get('txt_maloaisp')!;
-  // }
-  // get tenncc() {
-  //   return this.frmloaisp.get('txt_tenloaisp')!;
-  // }
 
-  // getLoaiSanPham() {
+  ngOnInit(): void {
+    this.LoadData();
+  }
+  public LoadData() {
+      this._api.get('/api/LoaiSanPham/get-loaisanpham').subscribe(res => {
+        this.list_loaisanphams = res;
+      });
+  }
 
-  //   // axios.get(environment.BASE_API + '/api/SanPham/get-all-sanpham').then(res => console.log(res)).catch(err => console.log(err));
+  get tenlsp() {
+    return this.frmLoaiSanPham.get('txt_tenlsp')!;
+  }
+  public createModal() {
+    this.showUpdateModal = true;
+    this.isCreate = true;
+    setTimeout(() => {
+      $('#createSanPhamModal').modal('toggle');
+      this.doneSetupForm = true;
+      this.frmLoaiSanPham = new FormGroup({
+        'txt_tenlsp': new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(250)]),
+      });
+    });
+  }
 
-  //   this.service.getLoaiSanPham(this.p)
-  //     .subscribe((response: any) => {
-  //       this.list_loaisanphams = response.data;
-  //       this.total = response.total;
-  //       // console.log(response);
-  //     });
 
-  // }
-  // pageChangeEvent(event: number) {
-  //   this.p = event;
-  //   this.getLoaiSanPham();
-  // }
+  public openUpdateModal(maSanPham: any) {
+    this.showUpdateModal = true;
+    this.doneSetupForm = false;
+    this.isCreate = false;
+    setTimeout(() => {
+      $('#createSanPhamModal').modal('toggle');
+      this._api.get('/api/loaisanpham/get-by-id/' + maSanPham).subscribe(res => {
+        this.sanpham = res[0];
+        this.frmLoaiSanPham = new FormGroup({
+          'txt_tenlsp': new FormControl(this.sanpham.TenLoaiSanPham, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]),
+        });
+        this.doneSetupForm = true;
+      });
+    });
+  }
+  public opendetailSP(maSanPham: any) {
+    this.showUpdateModal = true;
+    this.doneSetupForm = false;
+    this.isCreate = false;
+    setTimeout(() => {
+      $('#createSanPhamModal').modal('toggle');
+      this._api.get('/api/loaisanpham/get-by-id/' + maSanPham).subscribe(res => {
+        this.sanpham = res[0];
+        this.frmLoaiSanPham = new FormGroup({
+          'txt_tenlsp': new FormControl(this.sanpham.TenLoaiSanPham, [Validators.required, Validators.minLength(3), Validators.maxLength(250)]),
+        });
+        this.doneSetupForm = true;
+      });
+    });
+  }
+  public onRemove(MaLoaiSanPham: any) {
+    this._api.delete('/api/loaisanpham/delete-loaisanpham', MaLoaiSanPham).subscribe(res => {
+      alert('Xóa dữ liệu thành công');
+      this.LoadData();
+    });
+  }
+  public closeModal() {
+    $('#createSanPhamModal').closest('.modal').modal('hide');
+  }
+  public pwdCheckValidator(control: any) {
+    var filteredStrings = { search: control.value, select: '@#!$%&*' }
+    var result = (filteredStrings.select.match(new RegExp('[' + filteredStrings.search + ']', 'g')) || []).join('');
+    if (control.value.length < 6 || !result) {
+      return { matkhau: true };
+    } else {
+      return null;
+    }
+  }
+  ngAfterViewInit() {
+    this.loadScripts('assets/js/core/app.js', 'assets/js/pages/form_layouts.js');
 
+  }
+
+  pageChangeEvent(event: number) {
+    this.p = event;
+  }
+
+  public upload(event: any) {
+    if (event.target.files && event.target.files.length > 0) {
+      this.file = event.target.files[0];
+    }
+  }
+  public findInvalidControls() {
+    const invalid = [];
+    const controls = this.frmLoaiSanPham.controls;
+    for (const name in controls) {
+      if (controls[name].invalid) {
+        invalid.push(name);
+      }
+    }
+    return invalid;
+  }
+
+  OnSubmit(vl: any) {
+    console.log(this.findInvalidControls())
+    if (this.frmLoaiSanPham.invalid) {
+      return;
+    }
+    let obj: any = {};
+    obj.loaisanpham = {
+      TenLoaiSanPham: vl.txt_tenlsp,
+    };
+
+
+    if (this.isCreate) {
+      this._api.post('/api/sanpham/create-loaisanpham', obj).subscribe(res => {
+        console.log(res)
+        if (res.insertId > 0) {
+          alert('Thêm dữ liệu thành công');
+          this.LoadData();
+          this.closeModal();
+        } else {
+          alert('Có lỗi')
+        }
+      });
+    } else {
+      obj.loaisanpham.MaLoaiSanPham = this.sanpham.MaLoaiSanPham;
+      this._api.post('/api/sanpham/update-loaisanpham', obj).subscribe(res => {
+        if (res.affectedRows > 0) {
+          alert('Cập nhật dữ liệu thành công');
+          this.LoadData();
+          this.closeModal();
+        } else {
+          alert('Có lỗi')
+        }
+      });
+    }
+
+  }
 }
